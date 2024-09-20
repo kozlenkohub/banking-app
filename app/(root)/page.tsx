f44aac43a62ae0881df96b98c03df45ae096af68
Nikita Kozlenko <ggkozlenko@gmail.com>
2024-09-20 15:50:25 +0200

    routing + Card rightside

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
   const loggedIn = {
     firstName: 'Nikita',
+    lastName: 'Kolmogorov',
+    email: 'gg@gmail.com',
   };
   return (
     <section className="home">
@@ -16,9 +19,11 @@ const Home = () => {
             user={loggedIn.firstName || 'Guest'}
             subtext="Acces and manage your account and transactions efficiently."
           />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.23} />
         </header>
-        <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.23} />
+        Recent Trans
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
